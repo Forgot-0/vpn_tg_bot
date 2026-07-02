f3bc2003c34ff656ed96f70c80858c803cbe6c46
Forgot-0 <mr.aisamutdin@mail.ru>
2026-07-03 00:46:01 +0300

    small fix

--- app/application/queries/servers/list_servers.py
+++ app/application/queries/servers/list_servers.py
@@ -7,6 +7,8 @@ from app.application.dtos.servers import ServerDTO
 from app.application.dtos.users import UserJWTData
 from app.application.queries.base import BaseQuery, BaseQueryHandler
 from app.domain.repositories.servers import ServerFilter, ServerRepository
+from app.domain.services.access_control import RoleAccessControl
+from app.domain.values.users import UserRole
 
 
 @dataclass(frozen=True)
@@ -19,8 +21,12 @@ class ListServersQuery(BaseQuery):
 @dataclass(frozen=True)
 class ListServersQueryHandler(BaseQueryHandler[ListServersQuery, PaginatedResponseDTO[ServerDTO]]):
     server_repository: ServerRepository
+    access_service: RoleAccessControl
 
     async def handle(self, query: ListServersQuery) -> PaginatedResponseDTO[ServerDTO]:
+        if not self.access_service.can_action(query.user_jwt_data.role, UserRole.ADMIN):
+            raise 
+
         servers = await self.server_repository.list_filtered(filter=query.server_filter)
 
         return PaginatedResponseDTO(
